50e3d1702958269d472f56e5e7c8a4547b77c5ef
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-05 19:59:25 -0400

    feat: :sparkles: starting express server with mongoose and doenv

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import routes from './routes/routes';
+const PORT = process.env.PORT || 4000
+const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
+
+const app = express()
+
+app.use(express.json());
+app.use(cors())
+
+app.use(morgan('dev'));
+app.use(routes)
+
+
+const mongoURI =
+    process.env.MONGODB_URL || '';
+
+console.log('mongo attempt', mongoURI);
+mongoURI &&
+    mongoose
+        .connect(mongoURI)
+        .then(() => console.log('MongoDB connected...'))
+        .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${HOSTNAME}:${PORT}`)
+})
