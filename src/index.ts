ff66c4ec8b5c28c16d5ed0a1a462981761e19cd6
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-05 22:29:12 -0400

    feat: :bug: increasing limit for json file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan';
@@ -8,12 +10,13 @@ const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 
 const app = express()
 
-app.use(express.json());
 app.use(cors())
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded())
 
 app.use(morgan('dev'));
 app.use(routes)
-
 
 const mongoURI =
     process.env.MONGODB_URL || '';
