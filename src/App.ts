11c0d7eb998c2ffcac2ce41b68a47dea6cc01314
Artur Oliveira Gomes <artur.o.gomes@gmail.com>
2022-09-06 22:05:10 -0400

    refactor: :recycle: selecting environment for mongo url

--- src/App.ts
+++ src/App.ts
@@ -8,22 +8,19 @@ import routes from './routes/routes';
 
 const app = express()
 
-app.use(cors())
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded())
-
-app.use(morgan('dev'));
-app.use(routes)
-
 const mongoURI =
-    process.env.MONGODB_URL || '';
-
-console.log('mongo attempt', mongoURI);
+    (process.env.PROD ? process.env.MONGODB_URL : process.env.MONGODB_URL_TEST) || '';
+// console.log('mongo attempt', mongoURI);
 mongoURI &&
     mongoose
         .connect(mongoURI)
-        .then(() => console.log('MongoDB connected...'))
-        .catch((err) => console.log(err));
+        // .then(() => console.log('MongoDB connected...'))
+        .catch((err: any) => console.log(err));
+
+app.use(cors())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded())
+app.use(morgan('dev'));
+app.use(routes)
 
 export { app }
